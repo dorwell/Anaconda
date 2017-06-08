b8e4e23a129dc6c2a314a74c9277ac014069fe4e
dorwell <dorwell@gmail.com>
2017-06-08 20:23:14 +0800

    Sample code: pyplot

---
+++ pyplot.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 08 20:20:54 2017
+
+@author: wade
+"""
+
+import matplotlib.pyplot as plt
+plt.plot([1,2,3,4])
+plt.ylabel('some numbers')
+plt.show()
